299b184a0aeab8456f5a3ca1dcd62731901398df
Meegs96 <miguel.herrera.vindel96@gmail.com>
2023-10-05 14:48:49 -0400

    first commit

---
+++ workshop.js
@@ -0,0 +1,20 @@
+/* i will use the arithmetic operatiors to 
+solve for the lock combinations and then
+create an alert pop up that informs the user 
+of said combination to unlock it. */
+
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination.")
+// create
+
+
+let lockNumber1 = (400000/4000/10)
+//expcted outcome = 10
+console.log(lockNumber1)
+
+
+
+let lockNumber2 = (128**128/16 - 984)
+//expected outcome = 40 
+
+let lockNumber3 = (39*18/9/6+26)
+//expected outcome = 39 
